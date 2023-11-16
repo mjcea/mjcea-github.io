bf08b75cc21c943d12e53f877ca3324ad8a86350
mjcea <marcos.azevedo@unicap.br>
2023-11-16 20:26:40 -0300

    1611

---
+++ news-main/translate.py
@@ -0,0 +1,11 @@
+import openai
+openai.api_key = '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'
+messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
+while True:
+    message = input("User : ")
+    if message:
+        messages.append({"role": "user", "content": message}, )
+        chat = openai.ChatCompletion.create(  model="gpt-3.5-turbo", messages=messages)
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
